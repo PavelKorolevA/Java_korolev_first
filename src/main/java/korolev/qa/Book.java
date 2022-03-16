3ca8ceda08f2a99235b21adc131722e71a305313
p.korolev <amlerok96@gmail.com>
2022-03-16 21:50:41 +0300

    added FirstClass

---
+++ src/main/java/korolev/qa/Book.java
@@ -0,0 +1,10 @@
+package korolev.qa;
+
+public class Book {
+    String author = "Павел Королев";
+    String text = "Тестирование с нуля";
+
+    void doReadBook() {
+        System.out.println("книга прочитана! " + author);
+    }
+}
